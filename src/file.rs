5b2b61f0ff9a633c94e37d44fb4a4334a5c33972
Hasu <syu8.0324@outlook.jp>
2020-12-22 18:22:57 +0900

    improve format

--- src/file.rs
+++ src/file.rs
@@ -1,132 +1,125 @@
 use ansi_term::Colour;
-use serde::{Deserialize,Serialize};
-use std::fs::{self,File};
-use std::io::Write;
+use chrono::Utc;
+use serde::{Deserialize, Serialize};
+use std::fs::{self, File};
 use std::io;
+use std::io::Write;
+use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::process::Command;
-use std::os::unix::process::CommandExt;
-use chrono::Utc;
 
-
+extern crate dirs;
 extern crate serde;
 extern crate toml;
-extern crate dirs;
 
-const EXPAND:&str= ".md";
+const EXPAND: &str = ".md";
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Setting {
     editor: String,
-    path: String
+    path: String,
 }
 
-pub fn check_config_exsists(path:&str)->std::io::Result<()>{
-    if !Path::new(&path).exists(){
+pub fn check_config_exsists(path: &str) -> std::io::Result<()> {
+    if !Path::new(&path).exists() {
         generate_config()?;
     }
     Ok(())
 }
 
-
-fn generate_config()->std::io::Result<()>{
-
+fn generate_config() -> std::io::Result<()> {
     let raw_path = dirs::home_dir().unwrap();
     let path = raw_path.into_os_string().into_string().unwrap();
-    let _posts_dir = format!("{}{}",path,"/rlsmemo/_posts/");
-    let config_dir = format!("{}{}",path,"/rlsmemo/");
-
-
+    let _posts_dir = format!("{}{}", path, "/rlsmemo/_posts/");
+    let config_dir = format!("{}{}", path, "/rlsmemo/");
 
     check_dir_exsists(&config_dir)?;
     check_dir_exsists(&_posts_dir)?;
 
     let setting = Setting {
         editor: "vim".into(),
-        path:   _posts_dir.into(),
+        path: _posts_dir.into(),
     };
 
-
-    let config_file_path = format!("{}{}",config_dir,"Setting.toml");
+    let config_file_path = format!("{}{}", config_dir, "Setting.toml");
 
     let mut file = File::create(config_file_path)?;
     let toml = toml::to_string(&setting).unwrap();
-    write!(file,"{}",toml)?;
+    write!(file, "{}", toml)?;
     file.flush()?;
 
     Ok(())
-
-
 }
 
-fn need_input ()-> String{
+fn need_input() -> String {
     let mut word = String::new();
     io::stdin().read_line(&mut word).ok();
     return word.trim().to_string();
 }
 
-
-fn check_dir_exsists(path:&str)->std::io::Result<()>{
-    if !Path::new(&path).exists(){
+fn check_dir_exsists(path: &str) -> std::io::Result<()> {
+    if !Path::new(&path).exists() {
         fs::create_dir_all(&path)?;
     }
     Ok(())
 }
 
-pub fn open_editor(path:&str,title:String,editor:&str){
-    let filename = format!("{}{}",&path,&title);
+pub fn open_editor(path: &str, title: String, editor: &str) {
+    let filename = format!("{}{}", &path, &title);
 
     Command::new(editor).arg(filename).exec();
 }
 
-pub fn create_with_filename(path:&str,editor:&str,name:String)->std::io::Result<()>{
+pub fn create_with_filename(path: &str, editor: &str, name: String) -> std::io::Result<()> {
     check_dir_exsists(&path);
-    open_editor(&path,format!("{}{}",name,EXPAND),&editor);
+    open_editor(&path, format!("{}{}", name, EXPAND), &editor);
     Ok(())
 }
 
-
-
-pub fn create(path:&str,editor:&str){
-
+pub fn create(path: &str, editor: &str) {
     check_dir_exsists(&path);
 
     print!("Title :");
     io::stdout().flush().unwrap();
     let mut title = need_input();
 
-    if title.is_empty(){
-        title = format!("{}{}",Utc::now().format("%Y-%m-%d").to_string(),EXPAND);
-        println!("{}",title);
-    }else if !title.is_empty(){
-        title = format!("{}{}",title,EXPAND);
+    if title.is_empty() {
+        title = format!("{}{}", Utc::now().format("%Y-%m-%d").to_string(), EXPAND);
+        println!("{}", title);
+    } else if !title.is_empty() {
+        title = format!("{}{}", title, EXPAND);
     }
-    
-    open_editor(&path,title,&editor);
+
+    open_editor(&path, title, &editor);
 }
 
-pub fn delete(path:&str,filename:&str) -> std::io::Result<()>{
-
-    print!("{}",Colour::Red.paint("Are you sure you want to delete the file? (y/n):"));
+pub fn delete(path: &str, filename: &str) -> std::io::Result<()> {
+    print!(
+        "{}",
+        Colour::Red.paint("Are you sure you want to delete the file? (y/n):")
+    );
     io::stdout().flush().unwrap();
 
     let res = need_input();
 
-    if res == "y"{
-        fs::remove_file(format!("{}{}",&path,&filename)).unwrap();
-        let deleted_file_path = format!("{}{}",&path,&filename);
+    if res == "y" {
+        fs::remove_file(format!("{}{}", &path, &filename)).unwrap();
+        let deleted_file_path = format!("{}{}", &path, &filename);
 
-        println!("{}{}",Colour::Yellow.paint("Deleted :"),Colour::Yellow.paint(deleted_file_path));
+        println!(
+            "{}{}",
+            Colour::Yellow.paint("Deleted :"),
+            Colour::Yellow.paint(deleted_file_path)
+        );
     }
-        Ok(())
+    Ok(())
 }
 
-
-pub fn edit(path:&str,filename:&str,editor:&str){
+pub fn edit(path: &str, filename: &str, editor: &str) {
     let title = filename.to_string();
-    open_editor(&path,title,&editor);
+    open_editor(&path, title, &editor);
 }
 
-pub fn config(file_path:&str,editor:&str){
+pub fn config(file_path: &str, editor: &str) {
     Command::new(&editor).arg(&file_path).exec();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
-extern crate docopt;
 extern crate dirs;
+extern crate docopt;
 extern crate toml;
 use ansi_term::Colour;
 use docopt::Docopt;
-use serde::{Deserialize,Serialize};
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io::{BufReader, Read};
 use std::path::Path;
 
 pub mod file;
 
-
 #[derive(Debug, Deserialize)]
 pub struct Args {
     cmd_new: bool,
-    cmd_n:bool,
+    cmd_n: bool,
     cmd_list: bool,
-    cmd_l:bool,
+    cmd_l: bool,
     cmd_delete: bool,
-    cmd_d:bool,
-    cmd_config: bool, 
-    cmd_c:bool,
-    cmd_edit:bool,
-    cmd_e:bool,
+    cmd_d: bool,
+    cmd_config: bool,
+    cmd_c: bool,
+    cmd_edit: bool,
+    cmd_e: bool,
     arg_filename: String,
     flag_help: bool,
     flag_version: bool,
-    flag_t:String
+    flag_t: String,
 }
-
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Setting {
@@ -68,88 +66,92 @@ Options:
 
 ";
 
-
-
 fn main() {
-let raw_path = dirs::home_dir().unwrap();
+    let raw_path = dirs::home_dir().unwrap();
     let path = raw_path.into_os_string().into_string().unwrap();
-    let config_dir = format!("{}{}",path,"/rlsmemo/");
-    let config_file_path = format!("{}{}",config_dir,"Setting.toml");
-
+    let config_dir = format!("{}{}", path, "/rlsmemo/");
+    let config_file_path = format!("{}{}", config_dir, "Setting.toml");
 
     file::check_config_exsists(&config_file_path);
 
-    
-    let s = match read_file(format!("{}{}",config_dir,"Setting.toml").to_owned()) {
+    let s = match read_file(format!("{}{}", config_dir, "Setting.toml").to_owned()) {
         Ok(s) => s,
         Err(e) => panic!("fail to read file: {}", e),
     };
 
     let setting: Result<Setting, toml::de::Error> = toml::from_str(&s);
     let info = match setting {
-        Ok(p) =>  p,
+        Ok(p) => p,
         Err(e) => panic!("fail to parse toml: {}", e),
     };
 
     let full_path = info.path;
     let editor = info.editor;
 
-    let args: Args = Docopt::new(USAGE) .and_then(|d| d.deserialize())
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
+    if args.cmd_new && !args.flag_t.is_empty() || args.cmd_n && !args.flag_t.is_empty() {
+        file::create_with_filename(&full_path, &editor, args.flag_t);
+    }
+    if args.cmd_new || args.cmd_n {
+        println!("{}", args.arg_filename);
+        file::create(&full_path, &editor);
+    }
 
-    if args.cmd_new && !args.flag_t.is_empty() || args.cmd_n && !args.flag_t.is_empty(){
-        file::create_with_filename(&full_path,&editor,args.flag_t);
-    }
-    if args.cmd_new || args.cmd_n{
-        println!("{}",args.arg_filename);
-        file::create(&full_path,&editor);
-    }
-    
-    if args.cmd_list || args.cmd_l{
+    if args.cmd_list || args.cmd_l {
         list(&full_path);
     }
 
-    if args.cmd_delete||args.cmd_d{
-        file::delete(&full_path,&args.arg_filename);
+    if args.cmd_delete || args.cmd_d {
+        file::delete(&full_path, &args.arg_filename);
     }
 
-    if args.cmd_config || args.cmd_c{
-        file::config(&config_file_path,&editor);
+    if args.cmd_config || args.cmd_c {
+        file::config(&config_file_path, &editor);
     }
 
-    if args.cmd_e || args.cmd_edit{
-        file::edit(&full_path,&args.arg_filename,&editor);
+    if args.cmd_e || args.cmd_edit {
+        file::edit(&full_path, &args.arg_filename, &editor);
     }
 
-    if args.flag_version{
-        println!("{}",VERSION);
+    if args.flag_version {
+        println!("{}", VERSION);
     }
 
-
-    if !args.cmd_new && !args.cmd_n && !args.cmd_delete && !args.cmd_d && !args.cmd_list && !args.cmd_l && !args.cmd_c && !args.cmd_config&& args.arg_filename=="" && !args.flag_version{
-        println!("{}",USAGE);
+    if !args.cmd_new
+        && !args.cmd_n
+        && !args.cmd_delete
+        && !args.cmd_d
+        && !args.cmd_list
+        && !args.cmd_l
+        && !args.cmd_c
+        && !args.cmd_config
+        && args.arg_filename == ""
+        && !args.flag_version
+    {
+        println!("{}", USAGE);
     }
-
 }
 
+fn list(path: &str) {
+    let dir = fs::read_dir(&Path::new(&path)).unwrap();
+    let filenames = dir
+        .filter_map(|entry| {
+            entry.ok().and_then(|e| {
+                e.path()
+                    .file_name()
+                    .and_then(|n| n.to_str().map(|s| String::from(s)))
+            })
+        })
+        .collect::<Vec<String>>();
 
-fn list(path:&str){
-     let dir = fs::read_dir(&Path::new(&path)).unwrap();
-        let filenames  = dir.filter_map(|entry| {
-            entry.ok().and_then(|e|
-                e.path().file_name()
-                .and_then(|n| n.to_str().map(|s| String::from(s)))
-            )
-        }).collect::<Vec<String>>();
-
-        let list_color = Colour::RGB(84,189,199).on(Colour::Black);
-        for filename in filenames{
-            if filename.starts_with('.'){
-                continue;
-            }
-            println!("{}",list_color.paint(filename));
-
+    let list_color = Colour::RGB(84, 189, 199).on(Colour::Black);
+    for filename in filenames {
+        if filename.starts_with('.') {
+            continue;
         }
+        println!("{}", list_color.paint(filename));
+    }
 }
-
